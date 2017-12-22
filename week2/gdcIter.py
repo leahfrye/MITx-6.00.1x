38da2f78f989fb4848204a5af35e28d1cf1a76c8
leah <lefrye@nps.edu>
2017-12-21 16:24:04 -0800

    added gdcIter to week2

---
+++ week2/gdcIter.py
@@ -0,0 +1,16 @@
+# Finds the lowest common denominator between 2 numbers, iteratively
+
+def gdcIter(a, b):
+    smallest = min([a, b]);
+    largest = max([a, b]);
+
+    x = smallest;
+
+    while x > 0:
+        if largest % x == 0 and smallest % x == 0:
+            return x;  
+        else:
+            x -= 1;
+
+
+print(gdcIter(70, 100));
